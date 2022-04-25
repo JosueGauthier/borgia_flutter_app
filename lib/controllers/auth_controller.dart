82c637d3e03e52aa9ba0a30a101fa1d1c4986ea0
Josue Gauthier <josue.gauthier@gmail.com>
2022-04-25 21:24:11 +0200

    add authen method (not works)

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,1 +1,34 @@
+import 'package:borgiaflutterapp/data/repository/auth_repo.dart';
+import 'package:borgiaflutterapp/models/response_model.dart';
+import 'package:borgiaflutterapp/models/signup_body_model.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+class AuthController extends GetxController implements GetxService {
+  final AuthRepo authRepo;
+
+  AuthController({required this.authRepo});
+
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+
+  Future<ResponseModel> registration(SignUpBodyModel signUpBodyModel) async {
+    _isLoading = true;
+
+    Response response = await authRepo.registration(signUpBodyModel);
+    late ResponseModel responseModel; //late => before we use it we need to initialize it
+
+    if (response.statusCode == 200) {
+      authRepo.saveUserToken(response.body["token"]);
+      responseModel = ResponseModel(true, response.body["token"]);
+    } else {
+      responseModel = ResponseModel(false, response.statusText!);
+    }
+
+    _isLoading = true;
+
+    update();
+
+    return responseModel;
+  }
+}
