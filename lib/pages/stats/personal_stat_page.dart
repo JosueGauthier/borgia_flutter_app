65cbfc5c76a559022646ff265de0876fd6ae5c06
Josue Gauthier <josue.gauthier@gmail.com>
2022-05-23 19:33:47 +0200

    Row of chart

--- lib/pages/stats/personal_stat_page.dart
+++ lib/pages/stats/personal_stat_page.dart
@@ -1,10 +1,12 @@
 import 'package:borgiaflutterapp/controllers/user_shop_stat_controller.dart';
+import 'package:borgiaflutterapp/utils/dimensions.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../controllers/sale_list_controller.dart';
 import '../../utils/colors.dart';
 
+import '../../widget/stat_widget/custom_button_stat.dart';
 import '../../widget/stat_widget/piechart.dart';
 
 Map datetimeMap = {};
@@ -16,7 +18,31 @@ class MyStatPage extends StatefulWidget {
   State<MyStatPage> createState() => _MyStatPageState();
 }
 
+bool qtyButton = true;
+bool eurosButton = false;
+bool percentButton = false;
+
+changeGraphState(int buttonId) {
+  if (buttonId == 1) {
+    qtyButton = true;
+    eurosButton = false;
+    percentButton = false;
+  } else if (buttonId == 2) {
+    qtyButton = false;
+    eurosButton = true;
+    percentButton = false;
+  } else if (buttonId == 3) {
+    qtyButton = false;
+    eurosButton = false;
+    percentButton = true;
+  }
+}
+
 class _MyStatPageState extends State<MyStatPage> {
+  refresh() {
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return GetBuilder<SaleListController>(builder: (saleListController) {
@@ -29,27 +55,76 @@ class _MyStatPageState extends State<MyStatPage> {
             GetBuilder<UserShopStatController>(builder: (userShopStatController) {
               if (userShopStatController.isLoaded) {
                 List montantMagasins = userShopStatController.userShopStatList[0].montantMagasins;
-                return Column(
-                  children: [
-                    CustomPiechartWigdet(
-                      statList: montantMagasins,
-                      colorList: ListStatColors.colorslist1,
-                      isImagePresent: true,
-                      typeOfData: "quantity",
-                    ),
-                    CustomPiechartWigdet(
-                      statList: montantMagasins,
-                      colorList: ListStatColors.colors_12_list13,
-                      isImagePresent: true,
-                      typeOfData: "amount",
-                    ),
-                    CustomPiechartWigdet(
-                      statList: montantMagasins,
-                      colorList: ListStatColors.colors_12_list14,
-                      isImagePresent: true,
-                      typeOfData: "percentage",
-                    ),
-                  ],
+                return Container(
+                  margin: EdgeInsets.only(
+                    right: Dimensions.width10,
+                    left: Dimensions.width10,
+                  ),
+                  height: Dimensions.height100 * 3.1,
+                  width: double.maxFinite,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        height: double.maxFinite,
+                        width: Dimensions.width10 * 12,
+                        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                          CustomButtonStat(
+                            notifyParent: refresh,
+                            id: 1,
+                            size: Size(50, 30),
+                            buttonColor: ListStatColors.colorslist1[0],
+                          ),
+                          CustomButtonStat(
+                            notifyParent: refresh,
+                            id: 2,
+                            iconData: Icons.euro,
+                            size: Size(50, 30),
+                            buttonColor: ListStatColors.colorslist1[12],
+                          ),
+                          CustomButtonStat(
+                            notifyParent: refresh,
+                            id: 3,
+                            iconData: Icons.percent,
+                            size: Size(50, 30),
+                            buttonColor: ListStatColors.colorslist1[20],
+                          ),
+                        ]),
+                      ),
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            (qtyButton == true)
+                                ? Container(
+                                    //color: Colors.blueAccent,
+                                    child: CustomPiechartWigdet(
+                                      statList: montantMagasins,
+                                      colorList: ListStatColors.colorslist1,
+                                      isImagePresent: true,
+                                      typeOfData: "quantity",
+                                    ),
+                                  )
+                                : (eurosButton == true)
+                                    ? CustomPiechartWigdet(
+                                        statList: montantMagasins,
+                                        colorList: ListStatColors.colorslist1,
+                                        isImagePresent: true,
+                                        typeOfData: "amount",
+                                      )
+                                    : CustomPiechartWigdet(
+                                        statList: montantMagasins,
+                                        colorList: ListStatColors.colorslist1,
+                                        isImagePresent: true,
+                                        typeOfData: "percentage",
+                                      ),
+                            /* 
+                             */
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 );
               } else {
                 return Center(
---
+++ lib/widget/stat_widget/custom_button_stat.dart
@@ -0,0 +1,54 @@
+import 'package:borgiaflutterapp/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+import '../../pages/stats/personal_stat_page.dart';
+
+class CustomButtonStat extends StatefulWidget {
+  final Function() notifyParent;
+  final Color foregroundColor;
+  final Color buttonColor;
+  final String text;
+  final IconData? iconData;
+  final Size size;
+  final int id;
+
+  const CustomButtonStat({
+    Key? key,
+    required this.notifyParent,
+    this.foregroundColor = Colors.white,
+    this.text = "qty",
+    this.iconData,
+    required this.size,
+    this.buttonColor = AppColors.mainColor,
+    required this.id,
+  }) : super(key: key);
+
+  @override
+  State<CustomButtonStat> createState() => _CustomButtonStatState();
+}
+
+class _CustomButtonStatState extends State<CustomButtonStat> {
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(widget.buttonColor),
+        foregroundColor: MaterialStateProperty.all<Color>(widget.foregroundColor),
+        fixedSize: MaterialStateProperty.all<Size>(widget.size),
+      ),
+      onPressed: () {
+        setState(() {
+          widget.notifyParent();
+          changeGraphState(widget.id);
+        });
+      },
+      child: (widget.iconData == null)
+          ? Text(widget.text)
+          : Icon(
+              widget.iconData,
+              color: widget.foregroundColor,
+              size: widget.size.height / 1.5,
+            ),
+    );
+  }
+}
